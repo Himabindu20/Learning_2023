94435380970a74d82a5f04038dd12e6fd504180b
Himabindu20 <91364695+Himabindu20@users.noreply.github.com>
2023-06-22 10:43:42 +0530

    Add files via upload

---
+++ exercise3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void printBits(unsigned int num);
+
+int main() {
+    unsigned int num;
+    printf("Enter a 32-bit integer: ");
+    scanf("%u", &num);
+
+    printf("Bits: ");
+    printBits(num);
+
+    return 0;
+}
+
+void printBits(unsigned int num) {
+    int i;
+    unsigned int mask = 1 << 31; // Initialize mask with leftmost bit set
+
+    for (i = 0; i < 32; i++) {
+        unsigned int bit = (num & mask) ? 1 : 0;
+        printf("%u", bit);
+        mask >>= 1; // Shift mask to the right by 1 bit
+    }
+
+    printf("\n");
+}
